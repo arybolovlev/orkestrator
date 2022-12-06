37cdab3036a23339139cbc551ddf6ffb42f94165
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2022-12-06 16:35:31 +0100

    Add manager skeleton

---
+++ manager/manager.go
@@ -0,0 +1,31 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/arybolovlev/orkestrator/task"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDB        map[string][]task.Task
+	EventDB       map[string][]task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate worker")
+}
+
+func (m *Manager) UpdateTasks() {
+	fmt.Println("I will update tasks")
+}
+
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to workers")
+}
