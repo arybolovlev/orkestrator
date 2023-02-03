dee0c83f06535249393817d626ff9ee4595e504a
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2023-02-03 14:11:11 +0100

    Add initial Job and Task validators (#8)

--- job/job.go
+++ job/job.go
@@ -1,6 +1,8 @@
 package job
 
 import (
+	"errors"
+
 	"github.com/arybolovlev/orkestrator/task"
 )
 
@@ -14,4 +16,22 @@ func NewJob(n string, t []task.Task) *Job {
 		Name:  n,
 		Tasks: t,
 	}
+}
+
+func (j *Job) Validate() error {
+	if j.Name == "" {
+		return errors.New("Job Name must be set")
+	}
+
+	if len(j.Tasks) == 0 {
+		return errors.New("At least one Task must be set")
+	}
+
+	for _, t := range j.Tasks {
+		if err := t.Validate(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- task/task.go
+++ task/task.go
@@ -1,5 +1,9 @@
 package task
 
+import (
+	"errors"
+)
+
 type Image struct {
 	Name string
 	Tag  string
@@ -15,4 +19,16 @@ func NewTask(n string, i Image) *Task {
 		Name:  n,
 		Image: i,
 	}
+}
+
+func (t *Task) Validate() error {
+	if t.Name == "" {
+		return errors.New("Task Name must be set")
+	}
+
+	if t.Image.Name == "" {
+		return errors.New("Image Name must be set")
+	}
+
+	return nil
 }
